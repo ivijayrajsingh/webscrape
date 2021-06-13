2fe3ac9263af823207d39053704eac579ca8ddee
vijay raj singh <47878260+ivijayrajsingh@users.noreply.github.com>
2021-06-13 22:05:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,18 @@ Created on Sun Jun 13 21:13:27 2021
 
 from flask import Flask,Request,request,jsonify
 from scrape_linkedin import ProfileScraper
+from selenium import webdriver
+import os
 
 app = Flask(__name__)
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 @app.route('/index/')
 def index():
